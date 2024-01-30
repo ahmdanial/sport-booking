6774a4962cd3da8ccfd486f8086c09898304576a
Ahmad Danial <ahmaddani20037@gmail.com>
2024-01-30 09:07:40 +0800

    Remove debug_toolbar from INSTALLED_APPS and MIDDLEWARE

--- sport_booking/settings.py
+++ sport_booking/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_cleanup.apps.CleanupConfig',
-    "debug_toolbar",
     'Reservation'
 ]
 
@@ -52,7 +51,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
 ROOT_URLCONF = 'sport_booking.urls'
