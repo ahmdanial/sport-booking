94ed1e86dc945ccbe5ccbe8142b21c35801d06fd
Ahmad Danial <ahmaddani20037@gmail.com>
2024-01-30 08:39:29 +0800

    Refactor variable names for clarity

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn sport-booking.wsgi
--- sport_booking/settings.py
+++ sport_booking/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -143,3 +144,5 @@ STATICFILES_DIR = {
 
 MEDIA_ROOT =  os.path.join(BASE_DIR, 'media') 
 MEDIA_URL = '/media/'
+
+django_heroku.settings(locals())
